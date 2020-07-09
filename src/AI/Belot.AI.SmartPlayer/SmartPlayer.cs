5b7f585700296e77e0fb07b96f49525d2af665d6
Nikolay Kostov <admin@nikolay.it>
2020-07-09 15:46:45 +0300

    Implemented bids ranking and announcing in SmartPlayer (+362.58 ELO) ====================================================================== Belot Games Simulator 07/09/2020, Mode=Release, CPUs=24, OS=Microsoft Windows NT 6.2.9200.0, .NET=3.1.5 ====================================================================== Running SmartVsPreviousVersionGames... 00:00:10.0001241 Games: (SN-EW): 177930-22070 (Total: 200000, Diff: 155860) (Rounds: 3208090) ELO: 362.58 ====================================================================== Running SmartVsDummyGames... 00:00:16.3838199 Games: (SN-EW): 141301-58699 (Total: 200000, Diff: 82602) (Rounds: 17642579) ELO: 152.61 ====================================================================== Running SmartVsRandomGames... 00:00:05.8832564 Games: (SN-EW): 194571-5429 (Total: 200000, Diff: 189142) (Rounds: 2958051) ELO: 621.74 ====================================================================== Total tests time: 00:00:32.2703112

--- src/AI/Belot.AI.SmartPlayer/SmartPlayer.cs
+++ src/AI/Belot.AI.SmartPlayer/SmartPlayer.cs
@@ -14,17 +14,6 @@
     {
         private readonly TrickWinnerService trickWinnerService;
 
-        private readonly List<BidType> allBids = new List<BidType>
-                                                     {
-                                                         BidType.Pass,
-                                                         BidType.Clubs,
-                                                         BidType.Diamonds,
-                                                         BidType.Hearts,
-                                                         BidType.Spades,
-                                                         BidType.NoTrumps,
-                                                         BidType.AllTrumps,
-                                                     };
-
         public SmartPlayer()
         {
             this.trickWinnerService = new TrickWinnerService();
@@ -32,31 +21,41 @@
 
         public BidType GetBid(PlayerGetBidContext context)
         {
-            /*if (context.AvailableBids.HasFlag(BidType.AllTrumps))
+            if (context.AvailableBids.HasFlag(BidType.Clubs)
+                && CalculateTrumpBidPoints(context.MyCards, CardSuit.Club) >= 100)
             {
-                var allTrumpPoints = 0;
-                foreach (var card in context.MyCards)
-                {
-                    if (card.Type == CardType.Jack)
-                    {
-                        allTrumpPoints += 3;
-                    }
+                return BidType.Clubs;
+            }
 
-                    if (card.Type == CardType.Nine)
-                    {
-                        allTrumpPoints++;
-                    }
-                }
+            if (context.AvailableBids.HasFlag(BidType.Diamonds)
+                && CalculateTrumpBidPoints(context.MyCards, CardSuit.Diamond) >= 100)
+            {
+                return BidType.Diamonds;
+            }
 
-                if (allTrumpPoints >= 8)
-                {
-                    return BidType.AllTrumps;
-                }
-            }*/
+            if (context.AvailableBids.HasFlag(BidType.Hearts)
+                && CalculateTrumpBidPoints(context.MyCards, CardSuit.Heart) >= 100)
+            {
+                return BidType.Hearts;
+            }
 
-            return ThreadSafeRandom.Next(0, 2) == 0
-                       ? BidType.Pass // In 50% of the cases announce Pass
-                       : this.allBids.Where(x => context.AvailableBids.HasFlag(x)).RandomElement();
+            if (context.AvailableBids.HasFlag(BidType.Spades)
+                && CalculateTrumpBidPoints(context.MyCards, CardSuit.Spade) >= 100)
+            {
+                return BidType.Spades;
+            }
+
+            if (context.AvailableBids.HasFlag(BidType.AllTrumps) && CalculateAllTrumpBidPoints(context.MyCards) >= 100)
+            {
+                return BidType.AllTrumps;
+            }
+
+            if (context.AvailableBids.HasFlag(BidType.NoTrumps) && CalculateNoTrumpBidPoints(context.MyCards) >= 100)
+            {
+                return BidType.NoTrumps;
+            }
+
+            return BidType.Pass;
         }
 
         public IEnumerable<Announce> GetAnnounces(PlayerGetAnnouncesContext context)
@@ -81,6 +80,83 @@
 
         public void EndOfGame(GameResult gameResult)
         {
+        }
+
+        private static int CalculateAllTrumpBidPoints(CardCollection cards)
+        {
+            var bidPoints = 0;
+            foreach (var card in cards)
+            {
+                if (card.Type == CardType.Jack)
+                {
+                    bidPoints += 30;
+                }
+
+                if (card.Type == CardType.Nine)
+                {
+                    bidPoints += 15;
+                }
+
+                if (card.Type == CardType.Ace)
+                {
+                    bidPoints += 5;
+                }
+            }
+
+            return bidPoints;
+        }
+
+        private static int CalculateNoTrumpBidPoints(CardCollection cards)
+        {
+            var bidPoints = 0;
+            foreach (var card in cards)
+            {
+                if (card.Type == CardType.Ace)
+                {
+                    bidPoints += 30;
+                }
+
+                if (card.Type == CardType.Ten)
+                {
+                    bidPoints += 15;
+                }
+            }
+
+            return bidPoints;
+        }
+
+        private static int CalculateTrumpBidPoints(CardCollection cards, CardSuit trumpSuit)
+        {
+            var bidPoints = 0;
+            foreach (var card in cards)
+            {
+                if (card.Type == CardType.Jack && card.Suit == trumpSuit)
+                {
+                    bidPoints += 40;
+                }
+                else if (card.Type == CardType.Nine && card.Suit == trumpSuit)
+                {
+                    bidPoints += 30;
+                }
+                else if (card.Type == CardType.Ace && card.Suit == trumpSuit)
+                {
+                    bidPoints += 25;
+                }
+                else if (card.Suit == trumpSuit)
+                {
+                    bidPoints += 15;
+                }
+                else if (card.Type == CardType.Ace)
+                {
+                    bidPoints += 20;
+                }
+                else if (card.Type == CardType.Ten)
+                {
+                    bidPoints += 10;
+                }
+            }
+
+            return bidPoints;
         }
     }
 }
