50dab98eee24da662ce7e97f180af85099c819b6
Nikolay Kostov <admin@nikolay.it>
2020-08-15 16:52:28 +0300

    Add tests for BidTypeExtensions class. ====================================================================== Belot Games Simulator 08/15/2020, Mode=Release, CPUs=24, OS=Microsoft Windows NT 6.2.9200.0, .NET=3.1.6 ====================================================================== Running TwoSmartVsTwoPreviousVersionGames... 200000 Games: 99915-100085 (Δ -170) (Rounds: 3719341) ELO: -0.30 00:00:21.8271170 => Points: 27223k-27251k => Counters: 0,0,0,0,0,0,0,0 ====================================================================== Running TwoSmartVsTwoDummyGames... 200000 Games: 171323-28677 (Δ 142646) (Rounds: 5320152) ELO: 310.51 00:00:13.0158661 => Points: 31432k-20250k => Counters: 0,0,0,0,0,0,0,0 ====================================================================== Running OneSmartVsThreeDummyGames... 200000 Games: 154302-45698 (Δ 108604) (Rounds: 7839475) ELO: 211.39 00:00:13.6003422 => Points: 30415k-22431k => Counters: 0,0,0,0,0,0,0,0 ====================================================================== Running TwoSmartVsTwoRandomGames... 200000 Games: 197992-2008 (Δ 195984) (Rounds: 2245725) ELO: 797.55 00:00:07.9760442 => Points: 32957k-11508k => Counters: 0,0,0,0,0,0,0,0 ====================================================================== Running OneSmartVsThreeRandomGames... 200000 Games: 173217-26783 (Δ 146434) (Rounds: 2308331) ELO: 324.29 00:00:06.7155344 => Points: 31547k-18913k => Counters: 0,0,0,0,0,0,0,0 ====================================================================== Total tests time: 00:01:03.1604839

---
+++ src/Tests/Belot.Engine.Tests/Game/BidTypeExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Belot.Engine.Tests.Game
+{
+    using System;
+
+    using Belot.Engine.Cards;
+    using Belot.Engine.Game;
+    using Belot.Engine.Players;
+
+    using Xunit;
+
+    public class BidTypeExtensionsTests
+    {
+        [Theory]
+        [InlineData(BidType.Clubs, CardSuit.Club)]
+        [InlineData(BidType.Diamonds, CardSuit.Diamond)]
+        [InlineData(BidType.Hearts, CardSuit.Heart)]
+        [InlineData(BidType.Spades, CardSuit.Spade)]
+        [InlineData(BidType.Clubs | BidType.Double, CardSuit.Club)]
+        [InlineData(BidType.Diamonds | BidType.Double, CardSuit.Diamond)]
+        [InlineData(BidType.Hearts | BidType.Double, CardSuit.Heart)]
+        [InlineData(BidType.Spades | BidType.Double, CardSuit.Spade)]
+        [InlineData(BidType.Clubs | BidType.ReDouble, CardSuit.Club)]
+        [InlineData(BidType.Diamonds | BidType.ReDouble, CardSuit.Diamond)]
+        [InlineData(BidType.Hearts | BidType.ReDouble, CardSuit.Heart)]
+        [InlineData(BidType.Spades | BidType.ReDouble, CardSuit.Spade)]
+        public void ToCardSuitShouldWorkCorrectly(BidType bidType, CardSuit expectedCardSuit)
+        {
+            var actualResult = bidType.ToCardSuit();
+            Assert.Equal(expectedCardSuit, actualResult);
+        }
+
+        [Theory]
+        [InlineData(BidType.Pass)]
+        [InlineData(BidType.NoTrumps)]
+        [InlineData(BidType.AllTrumps)]
+        [InlineData(BidType.NoTrumps | BidType.Double)]
+        [InlineData(BidType.AllTrumps | BidType.Double)]
+        [InlineData(BidType.NoTrumps | BidType.ReDouble)]
+        [InlineData(BidType.AllTrumps | BidType.ReDouble)]
+        public void ToCardSuitShouldShouldThrowExceptionWhenGivenInvalidValues(BidType bidType)
+        {
+            Assert.Throws<ArgumentException>(() => bidType.ToCardSuit());
+        }
+    }
+}
